072fd13d26f6ede0fe5dc8f108886789b5f647af
Rockey Rocket <rocky.rocket.dev@gmail.com>
2024-07-10 09:31:12 +0200

    add route parameter

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ app.get("/api/courses", (reg, res) => {
   res.send([1, 2, 3]);
 });
 
+app.get("/api/courses/:id", (reg, res) => {
+  res.send(reg.params.id);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server running on port ${port}`));
