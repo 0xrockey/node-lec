aa43ee5a0dc29d699edeb5f692ca473c5d5b2228
Rockey Rocket <rocky.rocket.dev@gmail.com>
2024-07-10 09:24:50 +0200

    add env virable

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 app.get("/", (reg, res) => {
-  res.send("hello world");
+  res.send("Hello World");
 });
 app.get("/api/courses", (reg, res) => {
   res.send([1, 2, 3]);
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server running on port ${port}`));
