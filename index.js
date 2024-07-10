96e1ef927c56b4067966be9e37deaba7f61a3242
Rockey Rocket <rocky.rocket.dev@gmail.com>
2024-07-10 09:44:04 +0200

    handling Http GET request

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 app.get("/", (reg, res) => {
   res.send("Hello World");
 });
-app.get("/api/courses", (reg, res) => {
-  res.send([1, 2, 3]);
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
 });
 
-app.get("/api/courses/:id", (reg, res) => {
-  res.send(reg.params.id);
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("The courses with given ID was not found");
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
