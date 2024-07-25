d351011f00ea3ca5245a08726901196fd14a0483
Rockey Rocket <rocky.rocket.dev@gmail.com>
2024-07-25 16:09:35 +0200

    add movices action

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World! ");
+app.use(express.json());
+
+const genres = {
+  1: "Action",
+  2: "Comedy",
+  3: "Drama",
+};
+// routes put
+app.put("/api/genres/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const genre = {
+    id: id,
+    name: req.body.name,
+  };
+  genres[id] = genre;
+  res.json(genre);
 });
-app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+
+// routes post
+app.post("/api/genres", (req, res) => {
+  // check if genre already exists
+  if (genres[req.body.name]) {
+    return res.status(400).send("Genre already exists");
+  }
+  // validate genre use joi
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres[genre.id] = genre;
+  res.json(genre);
+});
+
+// routes get
+
+app.get("/", (req, res) => {
+  res.send("Vidle");
+});
+// get all genres
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+// get genres by id
+app.get("/api/genres/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.send(genres[id]);
 });
 
 const port = 3000;
